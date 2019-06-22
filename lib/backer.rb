8626b9f605043058fae8876e4b3d42ba4dde3c9c
Mark May <mamark90@gmail.com>
2019-06-22 19:57:23 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 class Backer
-attr_accessor :name
-attr_reader
+attr_accessor :name, :project 
+attr_reader :backed_projects
 
   def intitialize(name)
     @name = name
+    @backed_projects = []
 
   end
 
