b8296e8cea760a94acb79ddfff2c3deae0ad4a9d
Mark May <mamark90@gmail.com>
2019-06-23 00:46:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ attr_reader :backed_projects
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    project.add_backer(self) unless project.backers.include?(self) 
   end
 
 end
